74fc71f813b5c8501f61e38ae345b51cd5f2dd78
jcprice12 <jcprice12@gmail.com>
2017-07-26 23:43:56 -0400

    re-factored some more / bug fixing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -64,22 +64,22 @@ function Hangman(myWordsArray, myNumberOfGuesses, myRateOfChange, myPenalty, pla
 	//prints the word given to it. When I say "print" I mean it only prints
 	//the underscores for the letters in the word. The necessary word information
 	//(letters in the word) is saved in the wordDict object.
-	var instantiateWord = function(myWord){
-		deleteWord();
+	this.instantiateWord = function(myWord){
+		this.deleteWord();
 		for(i = 0; i < myWord.length; i++){
 			var myChar = myWord.charAt(i);
 			if(myChar.search(SPACE_REGEX) == -1){
-				var myCharElement = createChar(WHITESPACE);
-				var myElement = createCharContainer(myCharElement);
-				var myCharObj = createCharObj(myChar, myCharElement);
+				var myCharElement = this.createChar(WHITESPACE);
+				var myElement = this.createCharContainer(myCharElement);
+				var myCharObj = this.createCharObj(myChar, myCharElement);
 				wordDict.push(myCharObj);
 				document.getElementById("wordContainer").appendChild(myElement);
 			} else {
 				var emptySpace;
 				if(myChar === "-"){
-					emptySpace = createEmptySpace(myChar);
+					emptySpace = this.createEmptySpace(myChar);
 				} else {
-					emptySpace = createEmptySpace(WHITESPACE);
+					emptySpace = this.createEmptySpace(WHITESPACE);
 				}
 				document.getElementById("wordContainer").appendChild(emptySpace);
 			}
@@ -87,30 +87,30 @@ function Hangman(myWordsArray, myNumberOfGuesses, myRateOfChange, myPenalty, pla
 	};
 
 	//function that actually prints messages in the messageContainer element
-	var printMessage = function(message){
+	this.printMessage = function(message){
 		document.getElementById("messageContainer").innerHTML = message;
 	};
 
 	//print game won message for okayer
-	var printVictoryMessage = function(){
+	this.printVictoryMessage = function(){
 		var myMessage = "You won! Press a key to play again.";
-		printMessage(myMessage);
+		this.printMessage(myMessage);
 	};
 
 	//print a game over message for player
-	var printFailureMessage = function(){
+	this.printFailureMessage = function(){
 		var myMessage = "Darth Vader has vanquished you. Press a key to play again.";
-		printMessage(myMessage);
+		this.printMessage(myMessage);
 	};
 
 	//prints the amount of incorrect guesses you have left before you lose
-	var printRemainingGuesses = function(){
+	this.printRemainingGuesses = function(){
 		var myContainer = document.getElementById("guessesRemainingContainer");
 		myContainer.innerHTML = numberOfGuessesRemaining;
 	};
 
 	//prints the list of used incorrect characters. loops through array of usedCharacters
-	var printUsedCharacters = function(){
+	this.printUsedCharacters = function(){
 		var myContainer = document.getElementById("usedCharsContainer");
 		var myStringOfChars = "";
 		for(i = 0; i < usedCharacters.length; i++){
@@ -123,7 +123,7 @@ function Hangman(myWordsArray, myNumberOfGuesses, myRateOfChange, myPenalty, pla
 		myContainer.innerHTML = myStringOfChars;
 	};
 	//creates DOM for an empty space character (could be a hyphen)
-	var createEmptySpace = function(spaceChar){
+	this.createEmptySpace = function(spaceChar){
 		var myChar = document.createElement("div");
 		myChar.className = "myChar";
 		myChar.innerHTML = spaceChar;
@@ -134,7 +134,7 @@ function Hangman(myWordsArray, myNumberOfGuesses, myRateOfChange, myPenalty, pla
 	};
 
 	//creates DOM for character on-screen
-	var createChar = function (myCharVal){
+	this.createChar = function (myCharVal){
 		var myChar = document.createElement("div");
 		myChar.className = "myChar";
 		myChar.innerHTML = myCharVal;
@@ -142,23 +142,23 @@ function Hangman(myWordsArray, myNumberOfGuesses, myRateOfChange, myPenalty, pla
 	};
 
 	//creates DOM for underline below character on screen
-	var createUnderline = function(){
+	this.createUnderline = function(){
 		var myUnderline = document.createElement("div");
 		myUnderline.className = "myUnderline";
 		return myUnderline;
 	};
 
 	//creates DOM for container that holds both underline and character
-	var createCharContainer = function(myCharElement){
+	this.createCharContainer = function(myCharElement){
 		var myCharContainer = document.createElement("span");
 		myCharContainer.className="charContainer";
 		myCharContainer.appendChild(myCharElement);
-		myCharContainer.appendChild(createUnderline());
+		myCharContainer.appendChild(this.createUnderline());
 		return myCharContainer;
 	};
 
 	//deletes everything in the word container
-	var deleteWord = function(){
+	this.deleteWord = function(){
 		wordDict = [];
 		wordContainer = document.getElementById("wordContainer");
 		wordContainer.innerHTML = "";
@@ -168,18 +168,18 @@ function Hangman(myWordsArray, myNumberOfGuesses, myRateOfChange, myPenalty, pla
 	FUNCTIONS THAT AFFECT THE TIMER
 	****************************************************************/
 	//stop the timer (on game won or lost)
-	var stopTimer = function(){
+	this.stopTimer = function(){
 		clearTimeout(timerTimer);
 	};
 
 	//resets width of timer
-	var resetTimer = function(){
+	this.resetTimer = function(){
 		$(timerDiv).css({'width': '100%'});
 	};
 
 	//timer is re-drawn every time this function is called.
 	//this function can determine if the game is over
-	var tick = function(){
+	this.tick = function(){
 		//if time is not up
 		if(myPercentage !== 0){
 			myPercentage = myPercentage - currentRateOfChange;//current rate of change is mutable. size of timer width calculated here
@@ -190,18 +190,19 @@ function Hangman(myWordsArray, myNumberOfGuesses, myRateOfChange, myPenalty, pla
 			document.getElementById("myImage").style.opacity = opacityPercentage;
 			var localPercent = myPercentage + '%';
 			$(timerDiv).css({'width': localPercent});
-			timerTimer = setTimeout(function(){ 
-				tick();
+			var that = this;
+			timerTimer = setTimeout(function(){
+				that.tick();
 			}, 10);
 		} else { //time is up, round is over
 			this.roundOver();
-			executeLoseState();
+			this.executeLoseState();
 		}
 	};
 
 	//starts the timer animation
-	var startTimer = function(){
-		tick();
+	this.startTimer = function(){
+		this.tick();
 	};
 
 	/****************************************************************
@@ -211,7 +212,7 @@ function Hangman(myWordsArray, myNumberOfGuesses, myRateOfChange, myPenalty, pla
 	//charcters in a dictionary are essentially the characters in the word.
 	//the "key" is the corresponding letter in the word. The "value" is the actual DOM object.
 	//The value should equal the key for the character to be correct. 
-	var createCharObj = function(key, value){
+	this.createCharObj = function(key, value){
 		var coolChar = new Object();
 		coolChar.key = key.toUpperCase();
 		coolChar.value = value;
@@ -220,66 +221,46 @@ function Hangman(myWordsArray, myNumberOfGuesses, myRateOfChange, myPenalty, pla
 
 	//code to be executed on round over (win or lose)
 	this.roundOver = function(){
-		console.log("round over");
 		myPercentage = 100;
-		stopTimer();
+		this.stopTimer();
 		currentRateOfChange = RATE_OF_CHANGE;
 		correctCounter = 0;
 		numberOfGuessesRemaining = NUMBER_OF_GUESSES;
 		usedCharacters = [];
 		this.playing = false;
-		console.log(this.playing);
 	};
 
 	//code to be exectued after a win
-	var executeLoseState = function(){
-		printFailureMessage();
+	this.executeLoseState = function(){
+		this.printFailureMessage();
 	};
 
 	//code to be executed after a loss
-	var executeWinState = function(){
-		printVictoryMessage();
+	this.executeWinState = function(){
+		this.printVictoryMessage();
 	};
 
 	//code to be executed when a user inputs a NEW worng character. Can determine if game is over
-	var executeWrongGuess = function(upperChar){
+	this.executeWrongGuess = function(upperChar){
 		if(usedCharacters.indexOf(upperChar) === -1){
 			numberOfGuessesRemaining--;
 			currentRateOfChange = currentRateOfChange + PENALTY;
-			printRemainingGuesses();
+			this.printRemainingGuesses();
 			usedCharacters.push(upperChar);
-			printUsedCharacters();
+			this.printUsedCharacters();
 			if(numberOfGuessesRemaining <= 0){//hopefully it's never less than 0, but this will catch it if it does happen
 				this.roundOver();
-				executeLoseState();
+				this.executeLoseState();
 			}
 		}
 	};
 
 	//gets a random word from the word list array
-	var getWord = function(){
+	this.getWord = function(){
 		var word = MY_WORDS[Math.floor(Math.random() * MY_WORDS.length)];
 		return word;
 	};
 
-	/****************************************************************
-	HOUSE CLEANING
-	****************************************************************/
-	//essentially prepares the user for a new game
-	var houseCleaning = function(){
-		$( "#myImage" ).fadeTo( "fast" , 0, function() {
-	    // Code to be executed only after animation is completed
-	  });
-		printMessage("Press a key to guess a letter.");
-		printRemainingGuesses();
-		printUsedCharacters();
-		resetTimer();
-		startTimer();
-	};
-
-	/*****************************************************************************************************************
-	PUBLIC METHODS
-	******************************************************************************************************************/
 	//main bread and butter of the game. searches through chosen word for a matching character. can determine if game is over
 	this.findChars = function(myChar){
 		var charIsPresent = false;
@@ -292,7 +273,7 @@ function Hangman(myWordsArray, myNumberOfGuesses, myRateOfChange, myPenalty, pla
 					correctCounter++;
 					if(correctCounter == wordDict.length){//found all letters
 						this.roundOver();
-						executeWinState();
+						this.executeWinState();
 					}
 				}
 			}
@@ -300,15 +281,30 @@ function Hangman(myWordsArray, myNumberOfGuesses, myRateOfChange, myPenalty, pla
 
 		//if char was wrong and not seen before
 		if(!charIsPresent){
-			executeWrongGuess(myChar);
+			this.executeWrongGuess(myChar);
 		}
 	};
 
 	//calls necessary functions to start a new game
 	this.startOver = function(){
-		var myWord = getWord();
-		instantiateWord(myWord);
-		houseCleaning();
+		var myWord = this.getWord();
+		this.instantiateWord(myWord);
+		this.houseCleaning();
+	};
+
+	/****************************************************************
+	HOUSE CLEANING
+	****************************************************************/
+	//essentially prepares the user for a new game
+	this.houseCleaning = function(){
+		$( "#myImage" ).fadeTo( "fast" , 0, function() {
+	    // Code to be executed only after animation is completed
+	  });
+		this.printMessage("Press a key to guess a letter.");
+		this.printRemainingGuesses();
+		this.printUsedCharacters();
+		this.resetTimer();
+		this.startTimer();
 	};
 }
 
